1b3f2fd41e5535a401fa2386ed585f506ffe3fd2
Gleb Koldunov <g.koldunov@trinitydigital.ru>
2022-02-14 19:45:52 +0300

    refactoring(client): input from console

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"gRPCpet/pkg/api"
 	"google.golang.org/grpc"
 	"log"
@@ -14,26 +15,44 @@ func main() {
 	}
 	client := api.NewUserClient(conn)
 
-	//res, err := client.Create(context.Background(), &api.CreateRequest{
-	//	Name:     "test",
-	//	Email:    "test@mail.com",
-	//	Password: "password",
-	//})
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//log.Println(res.String())
+	//Create user method
+	log.Println("Create user request")
+	var name string
+	var email string
+	var password string
+	log.Print("Enter name: ")
+	fmt.Scanln(&name)
+	log.Print("Enter email: ")
+	fmt.Scanln(&email)
+	log.Print("Enter password: ")
+	fmt.Scanln(&password)
+	res, err := client.Create(context.Background(), &api.CreateRequest{
+		Name:     name,
+		Email:    email,
+		Password: password,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Create user response: ", res.String())
 
+	//Getting all users method
+	log.Println("Getting all users request...")
 	usersRes, err := client.Users(context.Background(), &api.Empty{})
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(usersRes.String())
+	log.Println("Get users response: ", usersRes.String())
 
-	delRes, err := client.Delete(context.Background(), &api.DeleteRequest{Id: 1})
+	//Delete user method
+	log.Println("Deleting user request...")
+	var id uint64
+	log.Print("Enter id: ")
+	fmt.Scanln(&id)
+	delRes, err := client.Delete(context.Background(), &api.DeleteRequest{Id: id})
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(delRes.String())
+	log.Println("Delete user response: ", delRes.String())
 
 }
